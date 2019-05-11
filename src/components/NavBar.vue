fa5ffc885655b6945158e47c90b87bd46b2373e1
Mahaoqu <mahaoqu@qq.com>
2019-05-11 16:57:43 +0800

    添加了导航栏和主页面的逻辑。修改异步操作，使用async/await语法。

---
+++ src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal"></el-menu>
+    <span>您好，{{info.name}}!</span>
+    <el-button @click="changeInfo" icon="el-icon-edit" type="text">修改个人信息</el-button>
+    <el-button @click="logout" round>登出</el-button>
+  </div>
+</template>
+
+<script>
+import { getUserInfo } from "@/api/userinfo";
+
+export default {
+  data() {
+    return {
+      info: {},
+      activeIndex: "1"
+    };
+  },
+  created() {
+    this.getInfo();
+  },
+  methods: {
+    async getInfo() {
+      this.info = await getUserInfo();
+    },
+    async logout() {
+      await this.$store.dispatch("LogOut");
+      location.reload(); // 重新实例化vue-router对象
+    },
+    changeInfo() {}
+  }
+};
+</script>
+
+<style>
+</style>
--- src/views/Dashbroad.vue
+++ src/views/Dashbroad.vue
@@ -1,20 +1,40 @@
 <template>
-  <div>
-    <el-button @click="logout" round>登出</el-button>
-  </div>
+  <el-container>
+    <el-header>
+      <NavBar></NavBar>
+    </el-header>
+    <el-main>Main</el-main>
+  </el-container>
 </template>
 
 <script>
+import NavBar from "@/components/NavBar";
+
 export default {
-  methods: {
-    logout() {
-      this.$store.dispatch("LogOut").then(() => {
-        location.reload(); // 重新实例化vue-router对象
-      });
-    }
-  }
+  components: {
+    NavBar
+  },
+
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {}
 };
 </script>
 
 <style>
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
 </style>
